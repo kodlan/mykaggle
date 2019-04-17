normalization issues fixed

titanic/titanic.py
@@ -3,6 +3,7 @@ import keras as K
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
 
 
 class MyLogger(K.callbacks.Callback):
@@ -16,10 +17,6 @@ class MyLogger(K.callbacks.Callback):
       print("epoch = %4d loss = %0.6f acc = %0.2f%%" % (epoch, curr_loss, curr_acc))
 
 
-def norm(data, column):
-    data[column] = (data[column] - data[column].mean()) / data[column].std()
-
-
 def categorize(data, column):
     data[column] = pd.Categorical(data[column])
     dfDummies = pd.get_dummies(data[column], prefix=column)
@@ -29,16 +26,12 @@ def categorize(data, column):
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1500)
 
-data = pd.read_csv(".\\data\\train.csv")
+data = pd.read_csv(".\\titanic\\data\\train.csv")
 
 # Fill all NaNs with 0
 data['Cabin'] = data['Cabin'].fillna('-')
 data = data.fillna(0)
 
-# Factorize
-# data['Sex'] = pd.factorize(data['Sex'])[0]
-# data['Cabin'] = pd.factorize(data['Cabin'])[0]
-# data['Embarked'] = pd.factorize(data['Embarked'])[0]
 
 # Categorize
 data = categorize(data, 'Pclass')
@@ -46,12 +39,6 @@ data = categorize(data, 'Sex')
 data = categorize(data, 'Embarked')
 data['Cabin'] = data['Cabin'].apply((lambda x: x[0] if len(x) > 0 else '0'))
 data = categorize(data, 'Cabin')
-
-# Normalize
-norm(data, 'Age')
-norm(data, 'SibSp')
-norm(data, 'Fare')
-norm(data, 'Parch')
 
 print(data.iloc[:10, :200])
 
@@ -61,6 +48,10 @@ X = data.as_matrix(columns=['Pclass_1', 'Pclass_2', 'Pclass_3', 'Sex_female', 'S
                             'Cabin_C', 'Cabin_D', 'Cabin_E', 'Cabin_F', 'Cabin_G', 'Cabin_T'])
 Y = data.as_matrix(columns=['Survived'])
 
+# Normalize
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
 print(X.shape)
 print(Y.shape)
 
@@ -69,13 +60,12 @@ model = Sequential()
 model.add(Dense(4, input_dim=22, kernel_initializer='normal', activation='relu'))
 model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer=K.optimizers.SGD(lr=0.001), metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer=K.optimizers.SGD(lr=0.01), metrics=['accuracy'])
 
 my_logger = MyLogger(n=100)
-h = model.fit(X, Y, batch_size=32, epochs=30000, verbose=0, callbacks=[my_logger])
+h = model.fit(X, Y, batch_size=10, epochs=500, verbose=0, callbacks=[my_logger])
 
-
-test = pd.read_csv(".\\data\\test.csv")
+test = pd.read_csv(".\\titanic\\data\\test.csv")
 
 # Fill all NaNs with 0
 test['Cabin'] = test['Cabin'].fillna('-')
@@ -88,11 +78,6 @@ test = categorize(test, 'Embarked')
 test['Cabin'] = test['Cabin'].apply((lambda x: x[0] if len(x) > 0 else '0'))
 test = categorize(test, 'Cabin')
 
-# Normalize
-norm(test, 'Age')
-norm(test, 'SibSp')
-norm(test, 'Fare')
-norm(test, 'Parch')
 
 test['Cabin_T'] = 0
 test['Embarked_0'] = 0
@@ -103,6 +88,8 @@ print(test.iloc[:10, :200])
 T = test.as_matrix(columns=['Pclass_1', 'Pclass_2', 'Pclass_3', 'Sex_female', 'Sex_male', 'Age', 'SibSp', 'Parch', 'Fare',
                             'Embarked_0', 'Embarked_C', 'Embarked_Q', 'Embarked_S', 'Cabin_-', 'Cabin_A', 'Cabin_B',
                             'Cabin_C', 'Cabin_D', 'Cabin_E', 'Cabin_F', 'Cabin_G', 'Cabin_T'])
+# Normalize
+T = sc.fit_transform(T)
 
 pred = model.predict(T)
 test['Survived'] = np.round(pred).astype(int)