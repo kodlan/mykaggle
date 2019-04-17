55acd00fad8e92f68abe783b8112908a5a082f27
kodlan <6258615+kodlan@users.noreply.github.com>
2019-04-17 10:01:05 +0300

    titanic binary classification

---
+++ titanic/titanic.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import keras as K
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+class MyLogger(K.callbacks.Callback):
+  def __init__(self, n):
+    self.n = n   # print loss & acc every n epochs
+
+  def on_epoch_end(self, epoch, logs={}):
+    if epoch % self.n == 0:
+      curr_loss =logs.get('loss')
+      curr_acc = logs.get('acc') * 100
+      print("epoch = %4d loss = %0.6f acc = %0.2f%%" % (epoch, curr_loss, curr_acc))
+
+
+def norm(data, column):
+    data[column] = (data[column] - data[column].mean()) / data[column].std()
+
+
+def categorize(data, column):
+    data[column] = pd.Categorical(data[column])
+    dfDummies = pd.get_dummies(data[column], prefix=column)
+    return pd.concat([data, dfDummies], axis=1)
+
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1500)
+
+data = pd.read_csv(".\\data\\train.csv")
+
+# Fill all NaNs with 0
+data['Cabin'] = data['Cabin'].fillna('-')
+data = data.fillna(0)
+
+# Factorize
+# data['Sex'] = pd.factorize(data['Sex'])[0]
+# data['Cabin'] = pd.factorize(data['Cabin'])[0]
+# data['Embarked'] = pd.factorize(data['Embarked'])[0]
+
+# Categorize
+data = categorize(data, 'Pclass')
+data = categorize(data, 'Sex')
+data = categorize(data, 'Embarked')
+data['Cabin'] = data['Cabin'].apply((lambda x: x[0] if len(x) > 0 else '0'))
+data = categorize(data, 'Cabin')
+
+# Normalize
+norm(data, 'Age')
+norm(data, 'SibSp')
+norm(data, 'Fare')
+norm(data, 'Parch')
+
+print(data.iloc[:10, :200])
+
+# Create np arrays
+X = data.as_matrix(columns=['Pclass_1', 'Pclass_2', 'Pclass_3', 'Sex_female', 'Sex_male', 'Age', 'SibSp', 'Parch', 'Fare',
+                            'Embarked_0', 'Embarked_C', 'Embarked_Q', 'Embarked_S', 'Cabin_-', 'Cabin_A', 'Cabin_B',
+                            'Cabin_C', 'Cabin_D', 'Cabin_E', 'Cabin_F', 'Cabin_G', 'Cabin_T'])
+Y = data.as_matrix(columns=['Survived'])
+
+print(X.shape)
+print(Y.shape)
+
+# Prepare model
+model = Sequential()
+model.add(Dense(4, input_dim=22, kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer=K.optimizers.SGD(lr=0.001), metrics=['accuracy'])
+
+my_logger = MyLogger(n=100)
+h = model.fit(X, Y, batch_size=32, epochs=30000, verbose=0, callbacks=[my_logger])
+
+
+test = pd.read_csv(".\\data\\test.csv")
+
+# Fill all NaNs with 0
+test['Cabin'] = test['Cabin'].fillna('-')
+test = test.fillna(0)
+
+# Categorize
+test = categorize(test, 'Pclass')
+test = categorize(test, 'Sex')
+test = categorize(test, 'Embarked')
+test['Cabin'] = test['Cabin'].apply((lambda x: x[0] if len(x) > 0 else '0'))
+test = categorize(test, 'Cabin')
+
+# Normalize
+norm(test, 'Age')
+norm(test, 'SibSp')
+norm(test, 'Fare')
+norm(test, 'Parch')
+
+test['Cabin_T'] = 0
+test['Embarked_0'] = 0
+
+print(test.iloc[:10, :200])
+
+# Create np arrays
+T = test.as_matrix(columns=['Pclass_1', 'Pclass_2', 'Pclass_3', 'Sex_female', 'Sex_male', 'Age', 'SibSp', 'Parch', 'Fare',
+                            'Embarked_0', 'Embarked_C', 'Embarked_Q', 'Embarked_S', 'Cabin_-', 'Cabin_A', 'Cabin_B',
+                            'Cabin_C', 'Cabin_D', 'Cabin_E', 'Cabin_F', 'Cabin_G', 'Cabin_T'])
+
+pred = model.predict(T)
+test['Survived'] = np.round(pred).astype(int)
+
+test.to_csv('output.csv', columns=['PassengerId', 'Survived'], index=False)
